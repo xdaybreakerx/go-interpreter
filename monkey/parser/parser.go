10325b7f894a1d9d891701401eb0b37591bf4498
Xander Salathe <xsalathe@gmail.com>
2024-09-07 22:16:28 +1000

    feat: parse and testing for calls

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -40,6 +40,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS:    SUM,
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN: 	CALL,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -79,6 +80,8 @@ func New(l *lexer.Lexer) *Parser {
 	// fn parsing
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
+	// call parsing
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -396,4 +399,34 @@ func (p *Parser) parseFunctionLiteral() ast.Expression {
 	lit.Body = p.parseBlockStatement()
 
 	return lit
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return args
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
 }
