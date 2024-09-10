6fec4e628df5d2d7d1bf3fafec8c117769e69fd7
Xander Salathe <xsalathe@gmail.com>
2024-09-10 13:39:00 +1000

    fix: cat ascii on parser errors

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -39,22 +39,11 @@ func Start(in io.Reader, out io.Writer) {
 	}
 }
 
-const MONKEY_FACE = `            __,__
-   .--.  .-"     "-.  .--.
-  / .. \/  .-. .-.  \/ .. \
- | |  '|  /   Y   \  |'  | |
- | \   \  \ 0 | 0 /  /   / |
-  \ '- ,\.-"""""""-./, -' /
-   ''-' /_   ^ ^   _\ '-''
-       |  \._   _./  |
-       \   \ '~' /   /
-        '._ '-=-' _.'
-           '-----'
-`
+const MISHA_FACE = "\n(.=^・ェ・^=)\n\n"
 
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, MONKEY_FACE)
-	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, MISHA_FACE)
+	io.WriteString(out, "Woops! We got caught up in some cat chaos here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
