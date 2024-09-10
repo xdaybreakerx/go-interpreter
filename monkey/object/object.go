3fbc92284730dcf02c206ea9bba62c728e695d12
Xander Salathe <xsalathe@gmail.com>
2024-09-10 12:38:46 +1000

    feat: evaluate integers

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -15,22 +15,21 @@ type Object interface {
 	Inspect() string
 }
 
+// integer object
 type Integer struct {
 	Value int64
 }
-
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
-
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
+// bool object
 type Boolean struct {
 	Value bool
 }
-
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
+// Null object
 type Null struct{}
-
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
