af3170f50ab4414ee2f173b6b275bbc97c394806
Xander Salathe <xsalathe@gmail.com>
2024-09-10 12:49:49 +1000

    feat: evaluate boolean values, and null

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -5,9 +5,15 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
-	
+
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
@@ -18,6 +24,9 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -31,4 +40,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
